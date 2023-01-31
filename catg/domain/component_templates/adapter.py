f1282e71d39cbf8d0daa60023e9c9689b5984136
marubontan <marubontan@gmail.com>
2023-01-30 21:32:07 -0300

    add input dto

--- catg/domain/component_templates/adapter.py
+++ catg/domain/component_templates/adapter.py
@@ -1,10 +1,18 @@
 from typing import TypedDict
-from ..dto.{{use_case}} import {{parsed_object}}OutputDto
+from ..dto.{{use_case}} import {{parsed_object}}InputDto, {{parsed_object}}OutputDto
 
-class {{parsed_object}}Body(TypedDict):
+
+class {{parsed_object}}RequestBody(TypedDict):
+    pass
+
+class {{parsed_object}}ResponseBody(TypedDict):
     pass
 
 class {{parsed_object}}Adapter:
     @classmethod
-    def {{use_case}}_output_dto_to_parsed_body({{use_case}}_output_dto: {{parsed_object}}OutputDto) -> {{parsed_object}}Body:
+    def {{use_case}}_request_body_to_output_dto({{use_case}}_request_body: {{parsed_object}}RequestBody) -> {{parsed_object}}InputDto:
+        pass
+
+    @classmethod
+    def {{use_case}}_output_dto_to_response_body({{use_case}}_output_dto: {{parsed_object}}OutputDto) -> {{parsed_object}}ResponseBody:
         pass
--- catg/domain/component_templates/use_case.py
+++ catg/domain/component_templates/use_case.py
@@ -1,10 +1,10 @@
-from ..dto.{{use_case}} import {{parsed_object}}OutputDto
+from ..dto.{{use_case}} import {{parsed_object}}InputDto, {{parsed_object}}OutputDto
 
 class {{parsed_object}}UseCase:
     def __init__(self):
         pass
     
-    def execute(self) -> {{parsed_object}}OutputDto:
+    def execute(self, {{use_case}}_input_dto: {{parsed_object}}InputDto) -> {{parsed_object}}OutputDto:
         pass
 
         
